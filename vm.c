d62dac15bce6935f72dbc2160fe5696dab7acaec
daniel gregory <daniel.a.gregory@raytheon.com>
2022-01-18 09:46:56 -0500

    chap16 scanner

--- vm.c
+++ vm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -78,10 +79,9 @@ printf("==== start run() ====\n");
 #undef READ_BYTE
 }
 
-InterpretResult interpret(Chunk* chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char* source) {
+  compile(source);
+  return INTERPRET_OK;
 }
 
 void push(Value value) {
